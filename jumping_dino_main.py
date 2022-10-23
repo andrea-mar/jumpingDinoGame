bb14020c7dcfc9e04bc4d280d3f2f5074c3fb0bf
andrea-mar <andreanistormar@gmail.com>
2022-10-23 10:24:23 +0100

    dinosaur jumps - no physics

--- jumping_dino_main.py
+++ jumping_dino_main.py
@@ -3,27 +3,29 @@ import pygame
 # initialize pygame modules
 pygame.init()
 
+# STAGE
 # set the stage/window dimentions
 stage_width = 800
 stage_hight = 300
 stage_size = (stage_width, stage_hight)
 # stage colour
 blue = 0, 0, 255
+# draw the stage on the screen
+stage = pygame.display.set_mode(stage_size)
+
+# GROUND
 # ground colour
 green = 0, 225, 0
 # coordinates and dimensions for the ground
 (left, top) = (0, stage_hight - 50)
 (ground_width, ground_height) = (stage_width, 50)
+
+# DINOSAUR
 # coordinates for the starting position of the dinosaur
 (dino_x, dino_y) = (20, 160)
-
-# draw the stage on the screen
-stage = pygame.display.set_mode(stage_size)
-
 # load the dinosaur image
 dinosaur = pygame.image.load("dinosaur.png")
 dinosaur = pygame.transform.scale(dinosaur, (100, 100))
-
 
 # the code below will be executed repeatedly until the player closes/quits the game
 while True:
@@ -31,10 +33,23 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
+    # update background colour
     stage.fill(blue)
     # draw the ground
     pygame.draw.rect(stage, green, pygame.Rect(left, top, ground_width, ground_height))
+    
+    # JUMP - no physics
+    keys = pygame.key.get_pressed()
+    # moves dinosaur up but not beyond the screen limit
+    if keys[pygame.K_SPACE] and dino_y > 0:
+            dino_y = dino_y - 5
+    # moves dinosaur down until it reaches the ground
+    elif dino_y < 160:
+        dino_y = dino_y + 5
+
     # update dinosaur on screen
     stage.blit(dinosaur, (dino_x, dino_y))
+
     # update the whole stage
     pygame.display.flip()
+ 
