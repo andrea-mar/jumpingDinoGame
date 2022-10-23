7f44f864ebbdf0f347559b81a160451be51372d8
andrea-mar <andreanistormar@gmail.com>
2022-10-23 08:20:38 +0100

    added dinosaur sprite on stage

--- jumping_dino_main.py
+++ jumping_dino_main.py
@@ -14,9 +14,15 @@ green = 0, 225, 0
 # coordinates and dimensions for the ground
 (left, top) = (0, stage_hight - 50)
 (ground_width, ground_height) = (stage_width, 50)
+# coordinates for the starting position of the dinosaur
+(dino_x, dino_y) = (20, 160)
 
 # draw the stage on the screen
 stage = pygame.display.set_mode(stage_size)
+
+# load the dinosaur image
+dinosaur = pygame.image.load("dinosaur.png")
+dinosaur = pygame.transform.scale(dinosaur, (100, 100))
 
 
 # the code below will be executed repeatedly until the player closes/quits the game
@@ -28,4 +34,7 @@ while True:
     stage.fill(blue)
     # draw the ground
     pygame.draw.rect(stage, green, pygame.Rect(left, top, ground_width, ground_height))
+    # update dinosaur on screen
+    stage.blit(dinosaur, (dino_x, dino_y))
+    # update the whole stage
     pygame.display.flip()
