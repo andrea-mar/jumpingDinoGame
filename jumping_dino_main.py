c9a6f8f0c29353b8ea878059633bb38b4408f18c
andrea-mar <andreanistormar@gmail.com>
2022-10-23 08:01:30 +0100

    draw the ground

--- jumping_dino_main.py
+++ jumping_dino_main.py
@@ -4,14 +4,20 @@ import pygame
 pygame.init()
 
 # set the stage/window dimentions
-stage_width = 600
+stage_width = 800
 stage_hight = 300
 stage_size = (stage_width, stage_hight)
 # stage colour
 blue = 0, 0, 255
+# ground colour
+green = 0, 225, 0
+# coordinates and dimensions for the ground
+(left, top) = (0, stage_hight - 50)
+(ground_width, ground_height) = (stage_width, 50)
+
 # draw the stage on the screen
-pygame.display.set_mode(stage_size).fill(blue)
-pygame.display.update()
+stage = pygame.display.set_mode(stage_size)
+
 
 # the code below will be executed repeatedly until the player closes/quits the game
 while True:
@@ -19,8 +25,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-
-    
-    
-
-
+    stage.fill(blue)
+    # draw the ground
+    pygame.draw.rect(stage, green, pygame.Rect(left, top, ground_width, ground_height))
+    pygame.display.flip()
